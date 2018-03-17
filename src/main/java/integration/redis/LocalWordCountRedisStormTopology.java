1f9dcc5e24d344a6500ce431554fbe858e07829f
Qin <245970829@qq.com>
2018-03-17 10:54:33 +0800

    使用storm完成词频统计分析，并将输出写入到redis中

---
+++ src/main/java/integration/redis/LocalWordCountRedisStormTopology.java
@@ -0,0 +1,187 @@
+package integration.redis;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.redis.bolt.RedisStoreBolt;
+import org.apache.storm.redis.common.config.JedisPoolConfig;
+import org.apache.storm.redis.common.mapper.RedisDataTypeDescription;
+import org.apache.storm.redis.common.mapper.RedisStoreMapper;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.ITuple;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * 使用storm完成词频统计分析，并将输出写入到redis中
+ * @author Qin
+ */
+public class LocalWordCountRedisStormTopology {
+
+    public static class DataSourceSpout extends BaseRichSpout {
+        private SpoutOutputCollector collector;
+
+        @Override
+        public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+            this.collector = collector;
+        }
+
+        /**
+         * 业务：
+         * 1） 读取指定目录的文件夹下的数据
+         * 2） 把每一行数据发射出去
+         */
+        @Override
+        public void nextTuple() {
+            // 获取.txt所有文件
+            Collection<File> files = FileUtils.listFiles(new File("C:/Users/qinzhen/Desktop/输出/storm"),
+                    new String[]{"txt"}, true);
+            for (File file : files) {
+                try {
+                    // 获取文件中的所有内容
+                    List<String> lines = FileUtils.readLines(file);
+                    for (String line : lines) {
+                        // 发射出去
+                        collector.emit(new Values(line));
+                    }
+
+                    // 数据处理完之后，改名，否则一直重复执行
+                    FileUtils.moveFile(file, new File(file.getAbsolutePath() + System.currentTimeMillis()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("line"));
+        }
+    }
+
+    /**
+     * 对数据进行分割
+     */
+    public static class SplitBolt extends BaseRichBolt {
+        private OutputCollector collector;
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+            this.collector = collector;
+        }
+
+        /**
+         * 业务逻辑：
+         * line： 对line进行分割，按照逗号
+         */
+        @Override
+        public void execute(Tuple input) {
+            String line = input.getStringByField("line");
+            String[] words = line.split(",");
+
+            for (String word : words) {
+                collector.emit(new Values(word));
+            }
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word"));
+        }
+    }
+
+    /**
+     * 词频汇总Bolt
+     */
+    public static class CountBolt extends BaseRichBolt {
+        private OutputCollector collector;
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+            this.collector = collector;
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        /**
+         * 业务逻辑：
+         * 1）获取每个单词
+         * 2）对所有单词进行汇总
+         * 3）输出
+         */
+        @Override
+        public void execute(Tuple input) {
+            String word = input.getStringByField("word");
+            Integer count = map.get(word);
+            if (count == null) {
+                map.put(word, 1);
+            } else {
+                map.put(word, count+1);
+            }
+
+            this.collector.emit(new Values(word, map.get(word)));
+            System.out.println("emit: " + word);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word", "count"));
+        }
+    }
+
+    /**
+     * 将输出写入到redis中
+     */
+    public static class WordCountStoreMapper implements RedisStoreMapper {
+        private RedisDataTypeDescription description;
+        private final String hashKey = "wordCount";
+
+        public WordCountStoreMapper() {
+            description = new RedisDataTypeDescription(
+                    RedisDataTypeDescription.RedisDataType.HASH, hashKey);
+        }
+
+        @Override
+        public RedisDataTypeDescription getDataTypeDescription() {
+            return description;
+        }
+
+        @Override
+        public String getKeyFromTuple(ITuple tuple) {
+            return tuple.getStringByField("word");
+        }
+
+        @Override
+        public String getValueFromTuple(ITuple tuple) {
+            return tuple.getIntegerByField("count").toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("DataSourceSpout", new DataSourceSpout());
+        builder.setBolt("SplitBolt", new SplitBolt()).shuffleGrouping("DataSourceSpout");
+        builder.setBolt("CountBolt", new CountBolt()).shuffleGrouping("SplitBolt");
+
+        // 添加负责写入redis的bolt
+        JedisPoolConfig poolConfig = new JedisPoolConfig.Builder()
+                .setHost("localhost").setPort(6379).build();
+        RedisStoreMapper storeMapper = new WordCountStoreMapper();
+        RedisStoreBolt storeBolt = new RedisStoreBolt(poolConfig, storeMapper);
+        builder.setBolt("storeBolt", storeBolt).shuffleGrouping("CountBolt");
+
+
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("LocalWordCountStormTopology", new Config(), builder.createTopology());
+    }
+}
