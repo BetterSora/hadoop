b14f577f7ec4c5b03989ffef48522d665b8220d7
Qin <245970829@qq.com>
2018-06-02 14:09:14 +0800

    update

--- src/main/java/mr/wcdemo/WordCount.java
+++ src/main/java/mr/wcdemo/WordCount.java
@@ -11,6 +11,8 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 
@@ -47,18 +49,18 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         //是否运行为本地模式，就是看这个参数值是否为local，默认就是local
-        conf.set("mapreduce.framework.name", "local");
+        //conf.set("mapreduce.framework.name", "local");
 
         //本地模式运行mr程序时，输入输出的数据可以在本地，也可以在hdfs上
         //到底在哪里，就看以下两行配置你用哪行，默认就是file:///
-        //conf.set("fs.defaultFS", "hdfs://shizhan:8020/");
-        conf.set("fs.defaultFS", "file:///");
+        //conf.set("fs.defaultFS", "hdfs://hadoop:8020/");
+        //conf.set("fs.defaultFS", "file:///");
 
         //运行集群模式，就是把程序提交到yarn中去运行
         //要想运行为集群模式，以下3个参数要指定为集群上的值
 		/*conf.set("mapreduce.framework.name", "yarn");
-		conf.set("yarn.resourcemanager.hostname", "shizhan");
-		conf.set("fs.defaultFS", "hdfs://shizhan:8020/");*/
+		conf.set("yarn.resourcemanager.hostname", "hadoop");
+		conf.set("fs.defaultFS", "hdfs://hadoop:8020/");*/
 
         Job job = Job.getInstance(conf);
 
@@ -82,9 +84,9 @@ public class WordCount {
         CombineTextInputFormat.setMinInputSplitSize(job, 2097152);*/
 
         //指定job的输入原始文件所在目录
-        FileInputFormat.setInputPaths(job, new Path("C:/Users/qinzhen/Desktop/输出/wordcount/input/words.txt"));
+        FileInputFormat.setInputPaths(job, new Path("./data/wordcount/input/words.txt"));
         //指定job的输出结果所在目录
-        FileOutputFormat.setOutputPath(job, new Path("C:/Users/qinzhen/Desktop/输出/wordcount/output"));
+        FileOutputFormat.setOutputPath(job, new Path("./data/wordcount/output"));
 
         //将job中配置的相关参数，以及job所用的java类所在的jar包，提交给yarn去运行
         boolean res = job.waitForCompletion(true);
